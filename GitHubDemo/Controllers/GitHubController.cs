c8fcaf1f5a4af8cc908469dc674f331b0738347c
Nader Saoudy <developernader@gmail.com>
2022-09-15 20:54:59 +0300

    new controller

---
+++ GitHubDemo/Controllers/GitHubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubDemo.Controllers
+{
+    public class GitHubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GitHubView()
+        {
+            return View();
+        }
+    }
+}
